bc6a99ad4e01f8262f03d37e0671f8bd919c252a
Kevin Sun <ksun.i@shattucklabs.com>
2024-08-21 11:46:04 -0400

    added some comments

--- AzureAD_Disabled.ps1
+++ AzureAD_Disabled.ps1
@@ -1,8 +1,8 @@
-$TenantAdminURL = "https://org-admin.sharepoint.com"
+$TenantAdminURL = "https://org-admin.sharepoint.com" #Change This
 
 #Credentials for PnPOnline
-$AdminUserName = "admin@email.com"
-$SecurePassword = Get-Content "encrypted/password/file/path" | ConvertTo-SecureString
+$AdminUserName = "admin@email.com" #Change This
+$SecurePassword = Get-Content "encrypted/password/file/path" | ConvertTo-SecureString #Change This
 $Credential = new-object -typename System.Management.Automation.PSCredential -argumentlist $AdminUserName, $SecurePassword
 
 Connect-PnPOnline -Url $TenantAdminURL -Credentials $Credential
@@ -15,16 +15,16 @@ table tr:hover {background-color: #ddd;}
 table th { padding-top: 12px; padding-bottom: 12px; text-align: left; background-color: #4CAF50; color: white;}
 </style>"
 
-$EmailTo = "email@email.com"
-$subject = "AzureAD Disabled Accounts Report"
+$EmailTo = "email@email.com" #Change This
+$subject = "AzureAD Disabled Accounts Report" #Change This
 
 #Credentials for ADWS 
-$AdminUserName2 = "admin@email.com"
-$SecurePassword2 = Get-Content "encrypted/password/file/path" | ConvertTo-SecureString
+$AdminUserName2 = "admin@email.com" #Change This
+$SecurePassword2 = Get-Content "encrypted/password/file/path" | ConvertTo-SecureString #Change This
 $Credential2 = new-object -typename System.Management.Automation.PSCredential -argumentlist $AdminUserName2, $SecurePassword2
 
-
-[string]$body = Get-ADUser -Server server.com -Credential $Credential2 -Filter {(Enabled -eq $False)} -Properties Name, EmailAddress, Enabled |
+#Change The ADWSServer
+[string]$body = Get-ADUser -Server ADWSServer.com -Credential $Credential2 -Filter {(Enabled -eq $False)} -Properties Name, EmailAddress, Enabled |
     Select-Object @{Label="Name";Expression={$_.Name}},
                   @{Label="Email";Expression={$_.EmailAddress}},
                   @{Label="Enabled";Expression={$_.Enabled}} | 
